24bad87555aa34f18feafd0774a16cab73d46f44
Sepehr79 <sepehrmsm1379@gmail.com>
2021-02-17 00:04:38 +0330

    Spring jdbc

--- src/main/java/doa/PersonDao.java
+++ src/main/java/doa/PersonDao.java
@@ -4,20 +4,48 @@ import beans.Person;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PersonDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    /**
+     * Injection by constructor
+     */
+    public PersonDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    /**
+     * Injection by setter
+     */
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public JdbcTemplate getJdbcTemplate(){
         return jdbcTemplate;
+    }
+
+    public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+        return namedParameterJdbcTemplate;
+    }
+
+    public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     public int savePerson(Person person){
@@ -48,4 +76,55 @@ public class PersonDao {
         });
     }
 
+    public List<Person> getPersons(){
+        return jdbcTemplate.query("select * from Person", new ResultSetExtractor<List<Person>>() {
+            @Override
+            public List<Person> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+                List<Person> people = new ArrayList<>();
+
+                while (resultSet.next()){
+                    String name = resultSet.getString("name");
+                    String lastName = resultSet.getString("lastName");
+                    int age = resultSet.getInt("age");
+                    int id = resultSet.getInt("id");
+
+                    Person person = new Person(name, lastName, age, id);
+                    people.add(person);
+                }
+
+                return people;
+            }
+        });
+    }
+
+    public List<Person> getPeopleRowMapper(){
+        return jdbcTemplate.query("select * from Person", new RowMapper<Person>() {
+            @Override
+            public Person mapRow(ResultSet resultSet, int index) throws SQLException {
+                Person person = new Person(resultSet.getString("name"), resultSet.getString("lastName"),
+                        resultSet.getInt("age"), resultSet.getInt("id"));
+
+                System.out.println(index);
+
+                return person;
+            }
+        });
+    }
+
+    public void saveByMap(Person person){
+        String query = "insert into Person values(:name, :lastName, :age, :id)";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", person.getName());
+        map.put("lastName", person.getLastName());
+        map.put("age", person.getAge());
+        map.put("id", person.getId());
+
+        namedParameterJdbcTemplate.execute(query, map, new PreparedStatementCallback<Integer>() {
+            @Override
+            public Integer doInPreparedStatement(PreparedStatement preparedStatement) throws SQLException, DataAccessException {
+                return preparedStatement.executeUpdate();
+            }
+        });
+    }
 }
--- src/test/java/main/Main.java
+++ src/test/java/main/Main.java
@@ -2,55 +2,68 @@ package main;
 
 import beans.Person;
 import doa.PersonDao;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class Main {
+
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+    PersonDao personDao = (PersonDao) applicationContext.getBean("dao");
+
+    @Before
+    public void clearDataBase(){
+        personDao.getJdbcTemplate().update("delete from Person");
+    }
+
     @Test
     public void testInsert(){
-
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        PersonDao personDao = (PersonDao) applicationContext.getBean("dao");
-
         int result = personDao.savePerson(new Person("sepehr", "mollaei", 20, 15));
-
         System.out.println(result);
-
-
     }
 
     @Test
     public void testDelete(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        PersonDao personDao = (PersonDao) applicationContext.getBean("dao");
-
         int result = personDao.deletePerson(new Person("sepehr", "mollaei", 20, 13));
-
         System.out.println(result);
     }
 
     @Test
     public void testUpdate(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        PersonDao personDao = (PersonDao) applicationContext.getBean("dao");
-
         int result = personDao.updatePerson(new Person("parham", "kabiri", 19, 12));
-
         System.out.println(result);
     }
 
     @Test
     public void testInsertPersonByPreparedStatement(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        PersonDao personDao = (PersonDao) applicationContext.getBean("dao");
-
         boolean result = personDao.insertPersonByPreparedStatement(new Person("ahmad", "salimi", 30, 456454));
-
         System.out.println(result);
+    }
+
+    @Test
+    public void testGetPeople(){
+        List<Person> people = personDao.getPersons();
+        Assert.assertEquals(people.size(), 0);
+    }
+
+    @Test
+    public void testGetPeopleRowMapper(){
+        personDao.savePerson(new Person("sepehr", "mollaei", 20, 9821160));
+        personDao.savePerson(new Person("sepehr", "mollaei", 20, 9821456));
+        personDao.savePerson(new Person("sepehr", "mollaei", 20, 982456));
+        personDao.savePerson(new Person("sepehr", "mollaei", 20, 9824560));
+
+        List<Person> people = personDao.getPeopleRowMapper();
+
+        Assert.assertEquals(people.size(), 4);
+    }
+
+    @Test
+    public void testSaveByMap(){
+        personDao.saveByMap(new Person("sepehr", "mollaei", 20, 9822116));
     }
 }
